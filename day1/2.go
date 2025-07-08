2f4a9b7a0fb1830ac0e841caeee4cd44c4ebc1c2
ali <traidwe@gmail.com>
2025-07-08 13:36:44 +0600

    first commit

---
+++ day1/2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b string = "da"
+	var c bool = true
+
+	fmt.Println(a, "old")
+	fmt.Println(b, "human?")
+	fmt.Println(c, "programmist&")
+}
